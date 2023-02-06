4e19603bdfb0169b01346369a13019bfc4f58d0d
Pekka Alaluukas <pekka.alaluukas@hotmail.com>
2023-02-06 15:19:02 +0200

    projekti alustettu

---
+++ src/main/java/com/mycompany/swingfrontend/SwingFrontend.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.swingfrontend;
+
+/**
+ *
+ * @author pekka
+ */
+public class SwingFrontend {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
